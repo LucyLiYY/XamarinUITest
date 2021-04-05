13c3e639156085e6c1364dd59a6ecab626dc4e31
Avocado <13241589646@163.com>
2021-04-05 20:18:04 +0800

    Add UI Test

---
+++ AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatform_UITest1
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile("C:/Users/yangl/Downloads/UPS Mobile_v9.1.0.10_apkpure.com.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace CrossPlatform_UITest1
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+       // [Test]
+        public void CreditCardNumber_TooLong_DisplayErrorMessage()
+        {
+            app.Repl();
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Continue"));
+            app.Tap(c => c.Marked("Continue"));
+
+           
+
+           app.WaitForElement(c => c.Marked("CheckBox"));
+           app.Tap(c => c.Marked("CheckBox"));
+
+            app.WaitForElement(c => c.Marked("Continue"));
+            app.Tap(c => c.Marked("Continue"));
+
+            app.WaitForElement(c => c.Marked("Sign Up for UPS ID"));
+            app.Tap(c => c.Marked("Sign Up for UPS ID"));
+
+
+             Assert.IsTrue(results.Any());
+        }
+    }
+}
